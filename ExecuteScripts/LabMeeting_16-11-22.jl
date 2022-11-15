ea7ef89aa201608a698d68d12f0745af3b78b636
Dario Sarra <dariosarra@gmail.com>
2022-11-15 15:01:16 +0000

    adjusted intervals

--- ExecuteScripts/LabMeeting_16-11-22.jl
+++ ExecuteScripts/LabMeeting_16-11-22.jl
@@ -32,28 +32,28 @@ unique(testb.Treatment)
 filter!(r->r.Treatment == "Baseline", testb)
 ## define x span
 @df testb density(:SummedForage./1000, xlims = (0,10))
-xspan = range(0,15000, step = 1000)
-findall(testb.SummedForage .< 10)
-open_html_table(testb[27804-10:27804+10,:])
+maxtime = 20000
+xspan = range(0,maxtime, step = 1000)
+gr(size = (400,400), xticks = 0:1:20)
 ## Naive survival
-naive_df = filter(r -> r.Leave && !r.Rewarded && r.SummedForage <=15000,testb)
+naive_df = filter(r -> r.Leave && !r.Rewarded && r.SummedForage <=maxtime,testb)
 naive_surv = combine(groupby(naive_df,:SubjectID)) do dd
                 dd2 = DataFrame(Bin = collect(xspan))
                 dd2[!, :Survival] = [sum(dd.SummedForage .> b)/nrow(dd) for b in dd2.Bin]
                 return dd2
         end
 naive_res = combine(groupby(naive_surv,:Bin), :Survival .=> [mean, sem])
-@df naive_res plot(:Bin/1000, :Survival_mean, ribbon = :Survival_sem)
+@df naive_res plot(:Bin/1000, :Survival_mean, ribbon = :Survival_sem, label = "Naive")
 ## Kaplan-Meier
 using Survival
 Distributions.fit(KaplanMeier,testb.SummedForage, testb.Rewarded)
-km_df = filter(r -> r.SummedForage <=15000,testb)
+km_df = filter(r -> r.SummedForage <=maxtime,testb)
 km_surv = combine(groupby(km_df,:SubjectID)) do dd
                 roundedforage = round.(dd.SummedForage./1000, digits = 0)
                 # Coding of event times is true for actual event time, false for right censored
                 km = Distributions.fit(KaplanMeier,roundedforage, .!dd.Rewarded)
                 dd2 = DataFrame(Bin = km.times, Survival = km.survival)
-                for v in maximum(dd2.Bin)+1:1:15
+                for v in maximum(dd2.Bin)+1:1:maxtime/1000
                         push!(dd2, (v, 0.0), promote=false) # add bins not calculated beyond max reached
                         push!(dd2,(0.0,1.0), promote=false) # add first bin starting at 1
                 end
@@ -61,4 +61,4 @@ km_surv = combine(groupby(km_df,:SubjectID)) do dd
         end
 open_html_table(km_surv)
 km_res = combine(groupby(km_surv,:Bin), :Survival .=> [mean, sem])
-@df km_res plot(:Bin, :Survival_mean, ribbon = :Survival_sem)
+@df km_res plot!(:Bin, :Survival_mean, ribbon = :Survival_sem, label = "Kaplan-Meier")
